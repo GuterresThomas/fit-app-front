e6e296b2d6ab816aafdcdd1317a582b296cb116f
Guterresthomas <guterresmikael@gmail.com>
2023-10-24 17:35:23 -0300

    login page: check

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -19,36 +19,45 @@ export default function Home() {
           if (response.ok) {
             const data = await response.json();
             console.log('Login successful:', data);
+            alert('Login efetuado!')
             // Faça o que você quiser com os dados de login, como redirecionar o usuário.
           } else {
             const errorData = await response.json();
             console.error('Login failed:', errorData);
+            
             // Lida com erros de login aqui.
           }
         } catch (error) {
           console.error('Erro ao fazer login:', error);
+          alert('Erro no login')
         }
       };
     
 
     
     return (
-      <div>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          require
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          require
-        />
-        <button onClick={handleLogin}>Login</button>
+      <div className="justify-center flex">
+        <div className="flex justify-center mt-20 h-80 bg-orange-400 rounded-2xl ">
+            <div className="flex flex-col align-middle justify-center p-2">
+                <input
+                className="m-2 bg-slate-100 rounded-md p-1 hover:bg-slate-200"    
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                require
+                />
+                <input
+                className="m-2 bg-slate-100 rounded-md p-1 hover:bg-slate-200"
+                type="password"
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                require
+                />
+                <button className=" bg-slate-100 rounded-md m-2 p-1 hover:bg-slate-200 font-bold uppercase "onClick={handleLogin}>Login</button>
+            </div>
+        </div>
       </div>
     );
 }
